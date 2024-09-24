982239ffc175ff839483cbae81dc6ebbfb9dc020
md.munna <mk8761174@gmail.com>
2024-09-24 17:55:46 +0600

    js done

---
+++ blog.js
@@ -0,0 +1,8 @@
+document.getElementById("blog").addEventListener("click",function(){
+   window.location.href ="/blog.html"
+})
+document.getElementById("home").addEventListener("click",function(){
+    window.location.href ="/home.html"
+ })
+
+
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-document.getElementById("btn-donate-money").addEventListener("click",function(){
+
+// js-section-1-start
+document.getElementById("btn-donate-money-1").addEventListener("click",function(){
 
 
-    const addMoney = getInputFieldById("add-money")
+    const addMoney = getInputFieldById("add-money-1")
 // validation
     if(addMoney <=0 || isNaN(addMoney)){
-document.getElementById("income-error").classList.remove("hidden");
+document.getElementById("income-error-1").classList.remove("hidden");
 return
     }
     
    const mainBalance = getTextFieldById("main-balance")
    
 
-    const balance = getTextFieldById("account-balance");
+    const balance = getTextFieldById("account-balance-1");
   const newBalance = balance + addMoney;
-  document.getElementById("account-balance").innerText = newBalance
+  document.getElementById("account-balance-1").innerText = newBalance
 
   const totalBalance =  mainBalance - addMoney
   document.getElementById("main-balance").innerText = totalBalance
@@ -46,7 +48,7 @@ historyBtn.addEventListener("click",function(){
     donationBtn.classList.remove("text-black","bg-arrow-primary","border","rounded-md")
     donationBtn.classList.add("text-gray-500")
 
-    document.getElementById("donate-section").classList.add("hidden")
+    document.getElementById("donate-section-1").classList.add("hidden")
 })
 
 donationBtn.addEventListener("click",function(){
@@ -57,7 +59,140 @@ donationBtn.classList.add(
 historyBtn.classList.remove(
   "text-black","bg-arrow-primary","border","rounded-md"
 )
-document.getElementById("donate-section").classList.remove("hidden");
+document.getElementById("donate-section-1").classList.remove("hidden");
 document.getElementById("transaction-history").classList.add("hidden")
 })
+//js-section-1-end
 
+
+
+// js-section-2-start
+document.getElementById("btn-donate-money-2").addEventListener("click",function(){
+
+
+    const addMoney = getInputFieldById("add-money-2")
+// validation
+    if(addMoney <=0 || isNaN(addMoney)){
+document.getElementById("income-error-2").classList.remove("hidden");
+return
+    }
+    
+   const mainBalance = getTextFieldById("main-balance")
+   
+
+    const balance = getTextFieldById("account-balance-2");
+  const newBalance = balance + addMoney;
+  document.getElementById("account-balance-2").innerText = newBalance
+
+  const totalBalance =  mainBalance - addMoney
+  document.getElementById("main-balance").innerText = totalBalance
+
+
+
+
+const div = document.createElement('div');
+div.innerHTML =`
+
+<p class ="font-bold border text-gray-500 p-4">${addMoney} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+<p class ="font-bold border text-gray-500 p-4"> Date:${new Date().toLocaleDateString()}</p>
+
+
+`;
+document.getElementById("transaction-history").appendChild(div);
+
+
+const modal = document.getElementById("my_modal_1");
+modal.showModal();
+modal.classList.remove("hidden")
+})
+
+// history btn
+const historyBtn2 = document.getElementById("btn-history");
+const donationBtn2 = document.getElementById("btn-donation");
+historyBtn.addEventListener("click",function(){
+    historyBtn.classList.add("text-black","bg-arrow-primary","border","rounded-md")
+    historyBtn.classList.remove("text-gray-500")
+    donationBtn.classList.remove("text-black","bg-arrow-primary","border","rounded-md")
+    donationBtn.classList.add("text-gray-500")
+
+    document.getElementById("donate-section-2").classList.add("hidden")
+})
+
+donationBtn.addEventListener("click",function(){
+donationBtn.classList.add(
+"text-black","bg-arrow-primary","border","rounded-md"
+
+);
+historyBtn.classList.remove(
+  "text-black","bg-arrow-primary","border","rounded-md"
+)
+document.getElementById("donate-section-2").classList.remove("hidden");
+document.getElementById("transaction-history").classList.add("hidden")
+})
+//js-section-2-end
+
+
+// js-section-3-start
+document.getElementById("btn-donate-money-3").addEventListener("click",function(){
+
+
+    const addMoney = getInputFieldById("add-money-3")
+// validation
+    if(addMoney <=0 || isNaN(addMoney)){
+document.getElementById("income-error-3").classList.remove("hidden");
+return
+    }
+    
+   const mainBalance = getTextFieldById("main-balance")
+   
+
+    const balance = getTextFieldById("account-balance-3");
+  const newBalance = balance + addMoney;
+  document.getElementById("account-balance-3").innerText = newBalance
+
+  const totalBalance =  mainBalance - addMoney
+  document.getElementById("main-balance").innerText = totalBalance
+
+
+
+
+const div = document.createElement('div');
+div.innerHTML =`
+
+<p class ="font-bold border text-gray-500 p-4">${addMoney} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+<p class ="font-bold border text-gray-500 p-4"> Date:${new Date().toLocaleDateString()}</p>
+
+
+`;
+document.getElementById("transaction-history").appendChild(div);
+
+
+const modal = document.getElementById("my_modal_1");
+modal.showModal();
+modal.classList.remove("hidden")
+})
+
+// history btn
+const historyBtn3 = document.getElementById("btn-history");
+const donationBtn3 = document.getElementById("btn-donation");
+historyBtn.addEventListener("click",function(){
+    historyBtn.classList.add("text-black","bg-arrow-primary","border","rounded-md")
+    historyBtn.classList.remove("text-gray-500")
+    donationBtn.classList.remove("text-black","bg-arrow-primary","border","rounded-md")
+    donationBtn.classList.add("text-gray-500")
+
+    document.getElementById("donate-section-3").classList.add("hidden")
+})
+
+donationBtn.addEventListener("click",function(){
+donationBtn.classList.add(
+"text-black","bg-arrow-primary","border","rounded-md"
+
+);
+historyBtn.classList.remove(
+  "text-black","bg-arrow-primary","border","rounded-md"
+)
+document.getElementById("donate-section-3").classList.remove("hidden");
+document.getElementById("transaction-history").classList.add("hidden")
+})
+//js-section-2-end
